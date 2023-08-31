dc4ebcebc0e766a0cd2118f2fd7d5cef4cb9468d
calvindany <calvindany1102@gmail.com>
2023-08-31 15:08:00 +0700

    First commit

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Soup_Backend.Models
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get;set; }
+        public Courses Courses { get; set; }
+    }
+}
