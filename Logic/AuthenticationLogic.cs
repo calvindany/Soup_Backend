b4d2b67a31c703e8400d9a13adf648b5b1458325
calvindany <calvindany1102@gmail.com>
2023-10-23 23:27:45 +0700

    Change Device

--- Logic/AuthenticationLogic.cs
+++ Logic/AuthenticationLogic.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace Soup_Backend.Logic
 {
@@ -8,9 +11,25 @@ namespace Soup_Backend.Logic
         public AuthenticationLogic(IConfiguration configuration) {
             _configuration = configuration;
         }
-        public string GenerateJWTBearer()
+        public string GenerateJWTBearer(String user)
         {
-            var key = Encoding.ASCII.GetBytes();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("JwtConfig:Key").Value)
+                );
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user)
+            };
+
+            var signingCredential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(120),
+                SigningCredentials = signingCredential
+            };
             return "";
         }
     }
